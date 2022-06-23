653bcd810ea2e4257a635a076121a8939e6a4816
basarYargici <ibrahimbyargici@gmail.com>
2022-06-24 00:14:57 +0300

    - navigationComponent added - new fragment added for webview

--- app/src/main/java/com/example/webintent/MainActivity.kt
+++ app/src/main/java/com/example/webintent/MainActivity.kt
@@ -1,44 +1,25 @@
 package com.example.webintent
 
-import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.browser.customtabs.CustomTabColorSchemeParams
-import androidx.browser.customtabs.CustomTabsIntent
-import androidx.core.content.ContextCompat
-import com.example.webintent.databinding.ActivityMainBinding
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
-    lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        setListeners()
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        setupActionBarWithNavController(navController)
     }
 
-    private fun setListeners() {
-        binding.btnCustomTab.setOnClickListener {
-            val url = "https://stackoverflow.com/questions/2808796/what-is-an-android-pendingintent"
-            val customTabsIntent = customTabIntentBuilder().build()
-            customTabsIntent.launchUrl(this, Uri.parse(url))
-        }
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
-
-    private fun customTabIntentBuilder(): CustomTabsIntent.Builder = CustomTabsIntent.Builder()
-        .setUrlBarHidingEnabled(true)
-        .setShowTitle(true)
-        .setShareState(CustomTabsIntent.SHARE_STATE_OFF)
-        .setDefaultColorSchemeParams(
-            CustomTabColorSchemeParams.Builder()
-                .setToolbarColor(
-                    ContextCompat.getColor(
-                        this,
-                        R.color.purple_200
-                    )
-                ).build()
-        )
 }
---
+++ app/src/main/java/com/example/webintent/WebFragment.kt
@@ -0,0 +1,18 @@
+package com.example.webintent
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class WebFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_web, container, false)
+    }
+}
